6399cd97d37482c3b19b53742c0d9c954d73e1fb
Bruno Alves Justino <brunojustino888@gmail.com>
2017-11-28 22:35:51 -0200

    Teste com webcam.
    Visual Studio Project para teste de manipulação de funções de uma webcam.

---
+++ WebCam Test I/webcam_js/Relogio.js
@@ -0,0 +1,9 @@
+﻿window.onload = function () {
+    setInterval(apresentarHoras, 1000);
+}
+
+function apresentarHoras() {
+    agora = new Date();
+    horas = agora.getHours() + ":" + agora.getMinutes() + ":" + agora.getSeconds();
+    document.getElementById("relogio").innerHTML = horas;
+}
