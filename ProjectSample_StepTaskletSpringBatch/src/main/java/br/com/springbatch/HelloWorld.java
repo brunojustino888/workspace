f4ef0ff80d63b7104727255d10af95ec5bc0c5d6
Bruno Alves Justino <brunojustino888@gmail.com>
2017-11-28 22:47:43 -0200

    Projeto spring-batch.
    Um simples projeto spring-batch.

---
+++ ProjectSample_StepTaskletSpringBatch/src/main/java/br/com/springbatch/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package br.com.springbatch;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+/**
+ * @author eyve2g
+ *
+ */
+public class HelloWorld implements Tasklet{
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		System.out.println("Hello World!");
+		return RepeatStatus.FINISHED;
+	}
+
+}
