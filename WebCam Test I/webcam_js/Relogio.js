cc7acad8cb2037cb3e27f7ac8791325896c8802f
Bruno Justino <brunojustino888@gmail.com>
2018-05-04 18:16:21 -0300

    Delete Relogio.js

--- WebCam Test I/webcam_js/Relogio.js
+++
@@ -1,9 +0,0 @@
-﻿window.onload = function () {
-    setInterval(apresentarHoras, 1000);
-}
-
-function apresentarHoras() {
-    agora = new Date();
-    horas = agora.getHours() + ":" + agora.getMinutes() + ":" + agora.getSeconds();
-    document.getElementById("relogio").innerHTML = horas;
-}
